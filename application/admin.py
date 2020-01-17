bf6daae1512ed188d248ce1a5bcb8d786063f903
Nowfel Mashnoor <nmmashnoor@gmail.com>
2020-01-17 21:46:42 +0600

    Added

--- application/admin.py
+++ application/admin.py
@@ -4,9 +4,53 @@ import json
 import face_recognition as fr
 import pymysql
 import base64
+import hmac
+import hashlib
+from phpserialize import loads, dumps
+
+from Crypto.Cipher import AES
 
 app = Flask(__name__)
 url = "/Users/mashnoor/clock_in_projects/afsclock/application/public/assets/faces/"
+key = b'GKUE0hDEQ69y6bEaBFrehXVKlxw4G5T31/gjQ4SLD6U='
+
+
+def mcrypt_decrypt(value, iv):
+    global key
+    AES.key_size = 128
+    crypt_object = AES.new(key=key, mode=AES.MODE_CBC, IV=iv)
+    return crypt_object.decrypt(value)
+
+
+def mcrypt_encrypt(value, iv):
+    global key
+    AES.key_size = 128
+    crypt_object = AES.new(key=key, mode=AES.MODE_CBC, IV=iv)
+    return crypt_object.encrypt(value)
+
+
+def decrypt(bstring):
+    global key
+    dic = json.loads(base64.b64decode(bstring).decode())
+    mac = dic['mac']
+    value = bytes(dic['value'], 'utf-8')
+    iv = bytes(dic['iv'], 'utf-8')
+    if mac == hmac.new(key, iv + value, hashlib.sha256).hexdigest():
+        return loads(mcrypt_decrypt(base64.b64decode(value), base64.b64decode(iv))).decode()
+    return ''
+
+
+def encrypt(string):
+    global key
+    iv = os.urandom(16)
+    string = dumps(string)
+    padding = 16 - len(string) % 16
+    string += bytes(chr(padding) * padding, 'utf-8')
+    value = base64.b64encode(mcrypt_encrypt(string, iv))
+    iv = base64.b64encode(iv)
+    mac = hmac.new(key, iv + value, hashlib.sha256).hexdigest()
+    dic = {'iv': iv.decode(), 'value': value.decode(), 'mac': mac}
+    return base64.b64encode(bytes(json.dumps(dic), 'utf-8'))
 
 
 def get_images_with_tag():
@@ -77,22 +121,22 @@ def face_match():
 @app.route('/face_rec', methods=['POST'])
 def face_recognition():
     image_data = str(request.form.get('image_data')).replace("data:image/jpeg;base64,", "")
-    #image_data = request.form.get('image_data')
+    # image_data = request.form.get('image_data')
     imgdata = base64.b64decode(image_data)
     with open("tmp.png", 'wb') as f:
         f.write(imgdata)
 
     file = open("tmp.png", 'rb')
 
-    name = face_rec(file)
-    #name = "hello"
-    print(name)
-    resp_data = {'name': name}
-    return json.dumps(resp_data)
+    id = face_rec(file)
+
+    print(id)
+    # resp_data = {'id': id}
+    return id
 
 
 # When debug = True, code is reloaded on the fly while saved
 
 
-#print(get_images_with_tag())
+# print(get_images_with_tag())
 app.run(port=5003, debug=True)
